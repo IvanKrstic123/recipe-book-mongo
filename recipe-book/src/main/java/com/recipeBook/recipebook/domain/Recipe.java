209cfd42804a9deaf7ae3b382678539d8da65153
Ivan Krstic <krstic8687@yahoo.com>
2021-06-06 23:01:00 +0200

    Many To Many relationship and H2 console start up. closes #2

--- recipe-book/src/main/java/com/recipeBook/recipebook/domain/Recipe.java
+++ recipe-book/src/main/java/com/recipeBook/recipebook/domain/Recipe.java
@@ -18,7 +18,7 @@ public class Recipe {
     private String directions;
     //todo add
 
-    @Lob        /** large object **/
+    @Lob /** large object **/
     private Byte[] image;
 
     @OneToOne(cascade = CascadeType.ALL)
@@ -27,6 +27,13 @@ public class Recipe {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")    /** propertie child-a,target **/
     private Set<Ingredient> ingredients;
 
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+        joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
+
+    
     public Long getId() {
         return id;
     }
@@ -109,5 +116,13 @@ public class Recipe {
 
     public Set<Ingredient> getIngredients() {
         return ingredients;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
