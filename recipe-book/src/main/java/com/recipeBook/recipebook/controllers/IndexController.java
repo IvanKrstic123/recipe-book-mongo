55c85f59c70352408a44982d54291aa8a80a5ad1
Ivan Krstic <krstic8687@yahoo.com>
2021-06-08 14:13:38 +0200

    Spring Data JPA Query Methods. closes #6

--- recipe-book/src/main/java/com/recipeBook/recipebook/controllers/IndexController.java
+++ recipe-book/src/main/java/com/recipeBook/recipebook/controllers/IndexController.java
@@ -1,14 +1,33 @@
 package com.recipeBook.recipebook.controllers;
 
+import com.recipeBook.recipebook.domain.Category;
+import com.recipeBook.recipebook.domain.UnitOfMeasure;
+import com.recipeBook.recipebook.repositories.CategoryRepository;
+import com.recipeBook.recipebook.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"","/","/index"})
     public String getIndexPage(){
-        System.out.println("Hello!!!! ");
+
+        Optional<Category> american = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> teaspoon = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println(american.get().getId());
+        System.out.println(teaspoon.get().getId());
         return "index.html";
     }
 }
