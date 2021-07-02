405ce922a2f0597e7c08d82587a2fc4e7d8ae99c
Ivan Krstic <krstic8687@yahoo.com>
2021-07-02 10:57:17 +0200

    Mockito Argument Captor

--- recipe-book/src/test/java/com/recipeBook/recipebook/controllers/IndexControllerTest.java
+++ recipe-book/src/test/java/com/recipeBook/recipebook/controllers/IndexControllerTest.java
@@ -1,18 +1,20 @@
 package com.recipeBook.recipebook.controllers;
 
-import com.recipeBook.recipebook.repositories.RecipeRepository;
+import com.recipeBook.recipebook.domain.Recipe;
 import com.recipeBook.recipebook.services.RecipeServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class IndexControllerTest {
 
@@ -35,10 +37,24 @@ class IndexControllerTest {
     @Test
     void getIndexPage() {
 
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
         String viewName = indexController.getIndexPage(model);
 
         assertEquals("index.html", viewName);
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet()); /* baca gresku bez eq */
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture()); /* baca gresku bez eq */
+
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
     }
 }
